ce8c38a5e24dc4658061f8a02fac179506ac62ac
Rayyze <chppleo@gmail.com>
2024-10-08 18:52:37 +0200

    adding style and test php

---
+++ test/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="icon" href="../../rayyze.png">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <p>
+            <?php 
+                echo date("H:i:s");
+            ?>
+        </p>
+    </body>
+</html> 
