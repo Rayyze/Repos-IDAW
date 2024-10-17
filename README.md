585b9b994c02544e588213382508f4e4b8d9007c
Rayyze <chppleo@gmail.com>
2024-10-17 20:50:21 +0200

    TP4 exo5 part 2 and api doc

--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# IDAW 2024
+# User Management API
+
+This API allows you to manage users in the database. You can create, read, update, and delete user records through simple HTTP requests.
+
+## Table of Contents
+
+1. [API Endpoints](#api-endpoints)
+    - [GET Users](#get-users)
+    - [POST New User](#post-new-user)
+    - [PUT Update User](#put-update-user)
+    - [DELETE User](#delete-user)
+2. [Response Format](#response-format)
+3. [Error Handling](#error-handling)
+4. [CORS Headers](#cors-headers)
+
+## API Endpoints
+
+The API supports the following HTTP methods and URL structures:
+
+### GET Users
+
+**Endpoint:**  
+`GET http://localhost/TP4/exo5/users.php`  
+or  
+`GET http://localhost/TP4/exo5/users.php/{id}` (where `{id}` is the user's ID)
+
+**Description:**  
+Fetches all users if no ID is specified. If an ID is provided, it fetches the user with that ID.
+
+### POST New User
+
+**Endpoint:**  
+`POST http://localhost/TP4/exo5/users.php/{login}/{email}`
+
+**Description:**  
+Creates a new user with the specified login and email and return a json with the new user, including its ID.
+
+### PUT Update User
+
+**Endpoint:**  
+`PUT http://localhost/TP4/exo5/users.php/{id}/{login}/{email}`
+
+**Description:**  
+Updates the user identified by `{id}` with the new login and email.
+
+**error**
+If a wrong ID or no ID is provided an error is returned along with an error code.
+
+### DELETE User
+
+**Endpoint:**  
+`DELETE http://localhost/TP4/exo5/users.php/{id}`
+
+**Description:**  
+Deletes the user identified by `{id}`.
+
+**Success**
+Success is indicated by a message field in json format.
+
+## Response Format
+
+All API responses are in JSON format. Here are examples of successful and error responses:
+
+### Successful Responses
+
+**Creating a User:**
+```json
+    {
+        "id": 1,
+        "login": "test",
+        "email": "test@test.com"
+    }
+```
+
+**Fetching users**
+```json
+    [
+        {
+            "id": 1,
+            "login": "test",
+            "email": "test@test.com"
+        },
+        {
+            "id": 2,
+            "login": "test2",
+            "email": "test2@test.com"
+        }
+    ]
+```
--- TP4/exo5/users.php
+++ TP4/exo5/users.php
@@ -7,13 +7,25 @@ function setHeaders() {
     header('Content-type: application/json; charset=utf-8');
 }
 
-function get_users($db) {
-    if(isset($_GET['id'])) {
+if(isset($_SERVER['PATH_INFO'])) {
+    $cleanedString = trim($_SERVER['PATH_INFO'], '/');
+    $inputArray = explode('/', $cleanedString);
+} else {
+    $inputArray = [
+        0 => '',
+        1 => '',
+        2 => ''
+    ];
+}
+
+function get_users($db, $id) {
+    if(isset($id) && $id != '') {
         $sql = "SELECT * FROM users WHERE id=:user_id"; 
         $exe = $db->prepare($sql);
     
-        $exe->bindParam(':user_id', $_GET['id']);
+        $exe->bindParam(':user_id', $id);
         $exe->execute();
+        $res = $exe->fetch(PDO::FETCH_OBJ);
     } else {
         $sql = "SELECT * FROM users"; 
         $exe = $db->query($sql); 
@@ -23,52 +35,101 @@ function get_users($db) {
     return $res;
 }
 
-function new_user($db) {
+function new_user($db, $login, $email) {
     $sql = "INSERT INTO users (login, email) VALUES (:login, :email)"; 
     $exe = $db->prepare($sql);
 
-    $exe->bindParam(':login', $_POST['login']);
-    $exe->bindParam(':email', $_POST['email']);
+    $exe->bindParam(':login', $login);
+    $exe->bindParam(':email', $email);
 
     if ($exe->execute()) {
         $newUserId = $db->lastInsertId();
         $res = [
             'id' => $newUserId,
-            'login' => $_POST['login'],
-            'email' => $_POST['email']
-        ];
-
+            'login' => $login,
+            'email' => $email];
         http_response_code(201);
     } else {
-        $res = [
-            "error" => "Failed to create user."
-        ];
+        $res = ["error" => "Failed to create user."];
         http_response_code(500);
     }
     return $res;
 }
 
+function update_user($db, $user_id, $login, $email) {
+    $sql = "UPDATE users SET login = :login, email = :email WHERE id = :id";
+    $exe = $db->prepare($sql);
+
+    $exe->bindParam(':login', $login);
+    $exe->bindParam(':email', $email);
+    $exe->bindParam(':id', $user_id);
+
+    if ($exe->execute()) {
+        $rowCount = $exe->rowCount();
+
+        if ($rowCount > 0) {
+            $res = [
+                'id' => $user_id,
+                'login' => $login,
+                'email' => $email];
+            http_response_code(200);
+        } else {
+            $res = ['error' => "User not found or no changes made."];
+            http_response_code(404);
+        }
+    } else {
+        $res = ["error" => "Failed to update user."];
+        http_response_code(500);
+    }
+
+    return $res;
+}
+
+function delete_user($db, $userId) {
+    $sql = "DELETE FROM users WHERE id = :user_id"; 
+    $exe = $db->prepare($sql);
+
+    $exe->bindParam(':user_id', $userId);
+
+    if ($exe->execute()) {
+        if ($exe->rowCount() > 0) {
+            $res = [
+                "message" => "User deleted successfully.",
+                "id" => $userId];
+            http_response_code(200);
+        } else {
+            $res = ["error" => "User not found."];
+            http_response_code(404);
+        }
+    } else {
+        $res = ["error" => "Failed to delete user."];
+        http_response_code(500);
+    }
+
+    return $res;
+}
+
 switch($_SERVER["REQUEST_METHOD"]) {
     case 'GET':
-        $result = get_users($pdo);
+        $result = get_users($pdo, $inputArray[0]);
         setHeaders();
         echo json_encode($result);
         exit;
         
     case 'POST':
-        $result = new_user($pdo);
+        $result = new_user($pdo, $inputArray[0], $inputArray[1]);
         echo json_encode($result);
         exit;
         
     case 'PUT':
-        $result = new_user($pdo);
-        http_response_code(501);
-        exit(json_encode(["error" => "PUT method not implemented"]));
+        $result = update_user($pdo, $inputArray[0], $inputArray[1], $inputArray[2]);
+        echo json_encode($result);
+        exit;
     
     case 'DELETE':
-        $result = new_user($pdo);
-        http_response_code(501);
-        exit(json_encode(["error" => "DELETE method not implemented"]));
+        $result = delete_user($pdo, $inputArray[0]);
+        echo json_encode($result);
+        exit;
         
     default:
         // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
